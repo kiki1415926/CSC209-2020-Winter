0b1145d08fb7c9f902d6d77d28602475bbd5ddc9
kiki <runqi.bi@mail.utoronto.ca>
2020-01-20 21:43:27 -0500

    finish benford

--- CSC209 A1/benford/benford.c
+++ CSC209 A1/benford/benford.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 #include "benford_helpers.h"
 
@@ -14,6 +15,28 @@ int main(int argc, char **argv) {
         fprintf(stderr, "benford position [datafile]\n");
         return 1;
     }
-
     // TODO: Implement.
+    int tmp;
+    int arr[BASE];
+    for(int i = 0 ; i < BASE ; i++){
+    	arr[i] = 0;
+    }
+    int count = 0;
+    // for(int i = 0 ; i < BASE ; i++){
+    // 	printf("%ds: %d\n", i, arr[i]);
+    // }
+    // printf("%d\n", atoi(argv[1]));
+    while (scanf("%d", &tmp) != EOF){
+    	// printf("%d\n", tmp);
+    	// for(int i = 0 ; i < BASE ; i++){
+	    // 	printf("%d\n", arr[i]);
+	    // }
+    	add_to_tally(tmp, atoi(argv[1]), arr);
+    	// for(int i = 0 ; i < BASE ; i++){
+	    // 	printf("%ds: %d\n", i, arr[i]);
+	    // }
+    }
+    for(int i = 0 ; i < BASE ; i++){
+	    printf("%ds: %d\n", i, arr[i]);
+	}
 }
--- CSC209 A1/benford/benford_helpers.c
+++ CSC209 A1/benford/benford_helpers.c
@@ -1,23 +1,48 @@
 #include <stdio.h>
+#include <math.h>
 
 #include "benford_helpers.h"
 
 int count_digits(int num) {
     // TODO: Implement.
-    return 0;
+    int count = 0;
+    while (pow(BASE, count) <= num){
+    	count++;
+    }
+    return count;
 }
 
 int get_ith_from_right(int num, int i) {
     // TODO: Implement.
-    return 0;
+    int count = 0;
+    int result = num;
+    int mode = 0;
+    while (count <= i){
+    	mode = result % BASE;
+    	result = result / BASE;
+    	count++;
+    }
+    return mode;
 }
 
 int get_ith_from_left(int num, int i) {
     // TODO: Implement.
-    return 0;
+    int len_num = count_digits(num);
+    // printf("len_num: %d\n", len_num);
+    i = len_num - i - 1;
+    // printf("i: %d\n", i);
+    return get_ith_from_right(num, i);;
 }
 
 void add_to_tally(int num, int i, int *tally) {
     // TODO: Implement.
+    int specific_pos = get_ith_from_left(num, i);
+    int count = 0;
+    // printf("specific_pos: %d\n", specific_pos);
+    while( count < specific_pos ){
+    	tally++;
+    	count++;
+    }
+    *tally += 1;
     return;
 }
